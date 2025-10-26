3a22b4f4a66c7a98db03d8536520367a431423ee
Júlia <juliascoite@gmail.com>
2025-10-26 13:52:03 -0300

    configurando a crew

--- crew.py
+++ crew.py
@@ -5,3 +5,39 @@ from crewai.knowledge.source.pdf_knowledge_source import PDFKnowledgeSource
 pdf_tool = PDFKnowledgeSource(
     file_paths=["bpf.pdf", "rdc301_2019.pdf", "rdc658_2022.pdf"]
 )
+
+llm = LLM(model="gpt-4o-mini", temperature=0)
+
+@CrewBase
+class ComplianceCrew:
+    agents_config = "config/agents.yaml"
+    tasks_config = "config/tasks.yaml"
+
+    @agent
+    def especialista_conpliance(self) -> Agent:
+        return Agent(
+            config=self.agents_config["especialista_compliance"],
+            verbose=True,
+            llm=llm,
+        )
+    
+    @task
+    def responder_pergunta_compliance(self) -> Task:
+        return Task(
+            config=self.tasks_config["responder_pergunta_compliance"],
+        )
+    
+    @crew
+    def crew(self) -> Crew:
+        """Cria o MQuestKnowledge crew"""
+
+        return Crew(
+            agents=[self.especialista_conpliance()],
+            tasks=[
+                self.responder_pergunta_compliance(),
+            ],
+            process=Process.sequential,
+            verbose=True,
+            knowledge_sources=[pdf_tool],
+        )
+
